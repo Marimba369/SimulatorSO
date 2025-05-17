b9f12c0fe95e475728466ea84d650de7dc6d8334
marimba369 <leonelfigueiracaetano@gmail.com>
2025-05-17 14:45:05 +0100

    change dispatcher and scheduler

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 
 int time = 0;
 
-void scheduler(Queue *QueueDestiny, Queue *Ready, Status status);
+void scheduler(Queue *QueueSource, Queue *Ready, Status status);
 Process *findByTime(Queue *Queue);
 Process *findByPID(Queue *Queue, int pid);
 
@@ -50,81 +50,67 @@ Process *findByPID(Queue *Queue, int pid)
 /*
     scheduler    
 */
-void scheduler(Queue *QueueDestiny, Queue *Ready, Status status)
+void scheduler(Queue *QueueSource, Queue *Ready, Status status)
 {
-    Process *process = findByTime(QueueDestiny); 
-    if( process == NULL ) //stop recursive call
-        return;
-    if( process->status == NEW )
+    while (1) 
     {
-        process = (Process *)dequeue(QueueDestiny);
-    }
-    else if ( process->status == BLOCKED )
-    {
-        process = removeNodeByData(QueueDestiny, process);  // remove in whatever position
-    }
-    else if ( process->status == EXIT )
-    {
-        Process *aux = dequeue(QueueDestiny);
-        killProcess(aux); // dump the process
-        scheduler(QueueDestiny, Ready, status);  //repeate while exists any process
-        return;
-    }
-    process->status = status;
-    enqueue(Ready, process);
+        Process *process = findByTime(QueueSource); 
+        if (process == NULL)
+            break;
 
-    scheduler(QueueDestiny, Ready, status);
+        // Remover o processo da fila dependendo do estado
+        if (process->status == NEW) 
+        {
+            process = (Process *)dequeue(QueueSource);
+        } 
+        else if (process->status == BLOCKED) 
+        {
+            process = removeNodeByData(QueueSource, process);
+        } 
+        else if (process->status == EXIT) 
+        {
+            Process *aux = dequeue(QueueSource);
+            killProcess(aux); // Libera recursos
+            continue; // Verifica se há mais processos
+        } 
+        else 
+            break;
+
+        // Atualiza estado e envia para fila READY
+        process->status = status;
+        enqueue(Ready, process);
+    }
 }
+
 
 /*
     dispatcher
 */
-void dispatcher(Process *processor, Queue *QueueDestiny, Status status)
+void dispatcher(Process **processor, Queue *QueueDestiny, Status status, Queue *QueueReady)
 {
-    processor->status = status;
-    processor->timeCpu = 0;
+    (*processor)->status = status;
+    (*processor)->timeCpu = 0;
     
     if( status == BLOCKED )
     {
-        int pc = processor->programCounter;
-        int timeEnqueue = (processor->context[pc] * -1) + time;
+        int pc = (*processor)->programCounter;
+        int timeEnqueue = ((*processor)->context[pc] * -1) + time;
 
-        processor->timeEnqueue = timeEnqueue;
+        (*processor)->timeEnqueue = timeEnqueue;
     }
     else if( status == EXIT )
     {
-        processor->timeEnqueue = time + 1;
+        (*processor)->timeEnqueue = time + 1;
     }
 
+    //change the context
     enqueue(QueueDestiny, processor);
+    *processor = (Process *)dequeue(QueueReady);
+    (*processor)->status = RUNNING;
 }   
 
 /*
-
-void printProcess(Process *processor, Queue *New, Queue *Blocked, Queue *Ready, Queue *Exit)
-{
-    printf("%-10d", time); 
-    for (int i = 1; i <= NUMBER_PROCESS; i++) 
-    {
-        if (processor != NULL && processor->pid == i) {
-            printf("%-15s", getStatus(processor->status));
-        } else {
-            Process *process = findByPID(Blocked, i);
-            if (process != NULL) {
-                printf("%-15s", getStatus(process->status));
-            } else if ((process = findByPID(Ready, i)) != NULL) {
-                printf("%-15s", getStatus(process->status));
-            } else if ((process = findByPID(New, i)) != NULL) {
-                printf("%-15s", getStatus(process->status));
-            } else if ((process = findByPID(Exit, i)) != NULL) {
-                printf("%-15s", getStatus(process->status));
-            } 
-        }
-    }
-    printf("\n");
-}
 */
-
 void printProcess(FILE *file, Process *processor, Queue *New, Queue *Blocked, Queue *Ready, Queue *Exit)
 {
     fprintf(file, "%-10d", time); 
@@ -203,76 +189,7 @@ int main()
     }
     fprintf(file, "\n");
 
-    while (time < TIME_LIMITE) 
-    {
-        time++;
-        printProcess(file, processor, NewQueue, BloquedQueue, ReadyQueue, ExitQueue);
 
-        scheduler(BloquedQueue, ReadyQueue, READY);
-        scheduler(NewQueue, ReadyQueue, READY);
-        scheduler(ExitQueue, ReadyQueue, EXIT);
-
-        if (processor == NULL) 
-        {
-            Process *auxProcess = (Process *)dequeue(ReadyQueue);
-            if (auxProcess != NULL) 
-            {
-                auxProcess->status = RUNNING;
-                processor = auxProcess;
-                processor->timeCpu = 2;
-            }
-        } else {
-            int index = processor->programCounter;
-            int pc = processor->context[index];
-
-            if (processor->timeCpu <= QUANTUM) 
-            {
-                if (processor->programCounter > COLUN) 
-                {
-                    dispatcher(processor, ExitQueue, EXIT);
-                    processor = NULL;
-                    continue;
-                } else {
-                    if (pc > 100 && pc < 200) 
-                    {
-                        int jump = pc % 100;
-                        processor->programCounter -= jump;
-                    } else if (pc > 200 && pc < 300) {
-                        int idx = pc % 100;
-                        if (globalPid <= MAX_PROCESS) 
-                        {
-                            Process *forkProcess = forks(programs[idx], &globalPid, time);
-                            if (forkProcess != NULL) enqueue(NewQueue, forkProcess);
-                        }
-                    } else if (pc < 0) {
-                        dispatcher(processor, BloquedQueue, BLOCKED);
-                        processor = NULL;
-                    } else if (pc == 0) {
-                        dispatcher(processor, ExitQueue, EXIT);
-                        processor = NULL;
-                    }
-                }
-            } else {
-                if (processor->programCounter > COLUN) 
-                {
-                    dispatcher(processor, ExitQueue, EXIT);
-                    processor = NULL;
-                    continue;
-                } else {
-                    dispatcher(processor, ReadyQueue, READY);
-                    processor = NULL;
-                }
-            }
-
-            if (processor != NULL) 
-            {
-                processor->programCounter += 1;
-                processor->timeCpu += 1;
-            }
-        }
-        if (isEmpty(NewQueue) && isEmpty(BloquedQueue) && isEmpty(ReadyQueue) && processor == NULL)
-            break;
-    }
     deleteQueue(ReadyQueue);
     deleteQueue(ExitQueue);
     deleteQueue(BloquedQueue);
